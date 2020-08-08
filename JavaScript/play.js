d1fa38198434a51548f94ac1e878ba5622abe417
Janaki <janakishreeja23@gmail.com>
2020-08-08 18:58:53 +0530

    commit

--- JavaScript/play.js
+++ JavaScript/play.js
@@ -61,7 +61,7 @@ getNewQuestion =()=>{
     if (availableQuestions.length === 0 || questionCounter >= MAX_QUESTIONS) {
         //go to the end page
         localStorage.setItem("mostRecentScore", score);
-        return window.location.assign('/end.html');
+        return window.location.assign('/HTML/end.html');
     }
     questionCounter++;
    // questionCounterText.innerText = `${questionCounter}/${MAX_QUESTIONS}`;
